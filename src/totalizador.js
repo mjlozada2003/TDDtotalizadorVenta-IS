ab84d43167ea4f28f6494de78a1d7d060ded517b
Maria Lozada <mariajesuslozada10@gmail.com>
2026-09-06 22:47:53 -0400

    Revert "fix: complementar funciones"
    This reverts commit e80a69efcc4fade6c5ed230d9ee28f7a21e68ce1.

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,22 +1,29 @@
 function calcularPrecioNeto(cantidad, precioUnitario){
   return cantidad * precioUnitario;
 }
+function obtenerImpuesto(codigo){
+    if(codigo === "TX") return "TX(6.25%)";
+    if(codigo === "UT") return "UT(6.65%)";
+    if(codigo === "NV") return "NV(8%)";
+    if(codigo === "AL") return "AL(4%)";
+    if(codigo === "CA") return "CA(8.25%)";
+}
 
 function impuestoAplicado(codigo, precioNeto) {
-    if(codigo === "TX") return "TX(6.25%):" + precioNeto * 0.0625;
-    if(codigo === "UT") return "UT(6.65%):" + precioNeto * 0.0665;
-    if(codigo === "NV") return "NV(8%):" + precioNeto * 0.08;
-    if(codigo === "AL") return "AL(4%):" + precioNeto * 0.04;
-    if(codigo === "CA") return "CA(8.25%):" + precioNeto * 0.0825;
+    if(codigo === "TX") return precioNeto * 0.0625;
+    if(codigo === "UT") return precioNeto * 0.0665;
+    if(codigo === "NV") return precioNeto * 0.08;
+    if(codigo === "AL") return precioNeto * 0.04;
+    if(codigo === "CA") return precioNeto * 0.0825;
 }
 
 function descuentoAplicado(precioNeto) {
-    if(precioNeto >= 1000 && precioNeto < 3000) return "(3%): " + (precioNeto * 0.03);
-    if(precioNeto >= 3000 && precioNeto < 7000) return "(5%): " + (precioNeto * 0.05);
-    if(precioNeto >= 7000 && precioNeto < 10000) return "(7%): " + (precioNeto * 0.07);
-    if(precioNeto >= 10000 && precioNeto < 30000) return "(10%): " + (precioNeto * 0.10);
-    if(precioNeto >= 30000) return "(15%): " + (precioNeto * 0.15);
-    else return "(0%): 0";
+    if(precioNeto >= 1000 && precioNeto < 3000) return precioNeto * 0.03;
+    if(precioNeto >= 3000 && precioNeto < 7000) return precioNeto * 0.05;
+    if(precioNeto >= 7000 && precioNeto < 10000) return precioNeto * 0.07;
+    if(precioNeto >= 10000 && precioNeto < 30000) return precioNeto * 0.10;
+    if(precioNeto >= 30000) return precioNeto * 0.15;
+    else return 0;
 }
 
 export { calcularPrecioNeto, impuestoAplicado, descuentoAplicado };
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -15,38 +15,38 @@ describe("Totalizador", () => {
   });
 
   it("deberia mostrar el impuesto aplicado al precio neto", () => {
-    expect(impuestoAplicado("TX", 60)).toEqual("TX(6.25%): 3.75");
+    expect(impuestoAplicado("TX", 60)).toEqual(3.75);
   });
   it("deberia mostrar el impuesto aplicado al precio neto", () =>{
-    expect(impuestoAplicado("UT", 60)).toEqual("UT(6.65%): 3.99");
+    expect(impuestoAplicado("UT", 60)).toEqual(3.99);
   });
   it("deberia mostrar el impuesto aplicado al precio neto", () =>{
-    expect(impuestoAplicado("NV", 60)).toEqual("NV(8%): 4.8");
+    expect(impuestoAplicado("NV", 60)).toEqual(4.8);
   });
   it("deberia mostrar el impuesto aplicado al precio neto", () =>{
-    expect(impuestoAplicado("AL", 60)).toEqual("AL(4%): 2.4");
+    expect(impuestoAplicado("AL", 60)).toEqual(2.4);
   });
   it("deberia mostrar el impuesto aplicado al precio neto", () =>{
-    expect(impuestoAplicado("CA", 60)).toEqual("CA(8.25%): 4.95");
+    expect(impuestoAplicado("CA", 60)).toEqual(4.95);
   });
 
   it("deberia ver el descuento aplicado al precio neto", () =>{
-    expect(descuentoAplicado(60)).toEqual("(0%): 0");
+    expect(descuentoAplicado(60)).toEqual(0);
   });
   it("deberia ver el descuento aplicado al precio neto", () =>{
-    expect(descuentoAplicado(1000)).toEqual("(3%): 30");
+    expect(descuentoAplicado(1000)).toEqual(30);
   });
   it("deberia ver el descuento aplicado al precio neto", () =>{
-    expect(descuentoAplicado(3000)).toEqual("(5%): 150");
+    expect(descuentoAplicado(3000)).toEqual(150);
   });
   it("deberia ver el descuento aplicado al precio neto", () =>{
-    expect(descuentoAplicado(8000)).toEqual("(7%): 560");
+    expect(descuentoAplicado(8000)).toEqual(560);
   });
   it("deberia ver el descuento aplicado al precio neto", () =>{
-    expect(descuentoAplicado(10000)).toEqual("(10%): 1000");
+    expect(descuentoAplicado(10000)).toEqual(1000);
   });
   it("deberia ver el descuento aplicado al precio neto", () =>{
-    expect(descuentoAplicado(30000)).toEqual("(15%): 4500");
+    expect(descuentoAplicado(30000)).toEqual(4500);
   });
 });
 
